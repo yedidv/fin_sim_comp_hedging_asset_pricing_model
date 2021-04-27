0246535e9cc7e2591516601f500a339854ba2b68
Dylan Moody <moodyd2@rpi.edu>
2021-04-27 19:20:26 -0400

    stochastic deltas

--- main_code.R
+++ main_code.R
@@ -3,15 +3,17 @@ library(tidyverse)
 library(Matrix) 
 library(plotly) 
 library(moments) 
-setwd("~/Desktop/msba/spring2021/fin_sim_comp_hedging_asset_pricing_model")
+setwd("ba/fcs/project/fin_sim_comp_hedging_asset_pricing_model/fin_sim_comp_hedging_asset_pricing_model-main")
 source('asset_price_model.r') 
 source('misc_funs.r') 
+source('stochastic_volatility.r')
 source('geom_brownian_motion.r') 
 
 
 
+
 ## Read the data #### 
-n_stocks <- 2
+n_stocks <- 6
 prices <- Read_Data(n_stocks)
 prices %>% head() 
 
@@ -32,10 +34,10 @@ mu<- matrix(mean_rets, ncol  = 1)
 Sigma <- var(rets %>% select(-Date), use = 'complete.obs') 
 rho <- cor(rets %>% select(-Date), use = 'complete.obs') 
 sigma <- var(rets %>% select(-Date), use = 'complete.obs') 
-M <- 5 ## Number of paths 
-N <- 7 ## number of time steps 
+M <- 10 ## Number of paths 
+N <- 13 ## number of time steps 
 d <- n_stocks 
-
+t <- 1 
 ## Strike Price listed as Last value of all stocks 
 K <- prices %>% select(-Date) %>% tail(1) %>% t() %>% mean() 
 ## Unable to start at appropriate starting prices 
@@ -43,37 +45,33 @@ X0 <- prices %>% select(-Date) %>% tail(1)
 ## Only works when start returns are at a arbritrary number 
 X0 <- prices %>% select(-Date) %>% tail(1) %>% t() %>% mean() 
 
-## Brownian Motion model. 
-gmd_model <- Geom_Brownian(M, N, n_stocks, 30, mu, X0, Sigma) 
+gmd_model <- Geom_Brownian(M, N, n_stocks, t, mu, X0, Sigma) 
 
-stock_a <- gmd_model$X[,,2] 
-deltas_a <- gmd_model$Delta[,2]
-deltas_a
+gmd_model$Delta
+gmd_model$Option_Type
+gmd_model$X
 
+snp <- getSymbols('^GSPC', src = 'yahoo', from = as.Date('2010-1-1'), 
+                  to = as.Date('2021-1-1'), auto.assign = F) 
+snp
+snp <- as.data.frame(snp)
 
-
-## Matrix of Positions
-CF <- matrix(NA, ncol=N+1,nrow=M) 
-
-CF[,1] <- deltas_a[1] * stock_a[,1] 
-for(i in 1:N){
-  CF[,i + 1] <- -1*(deltas_a[i + 1] - deltas_a[i]) * stock_a[,i + 1] 
-}
-CF
-
-deltas_a[1] * stock_a[,1]
-
-dim((abs(var(CF))) ^ 0.5)
-dim(stock_a) 
-dim(CF) 
-dim(var(CF)) 
-
-BLS <- function(M,N,S0,K,r,sigma,t,mu){
-  print(N)
+snp.ret <- diff(snp[,6])/lag(snp[,6])
+snp.ret
+rets <- as.data.frame(rets)
+prices <- as.data.frame(prices)
+a <- b <- S0 <- V0 <- volvol <- array(NA, dim=c(d))
+delts <- array(NA, dim=c(d,M))
+for(i in 1:d){
+  result <- lm(rets[,i+1]~snp.ret[2:2769])
+  a[i] <- result$coefficients[1]
+  b[i] <- result$coefficients[2]
+  V0[i] <- sd(rets[,i+1])
+  S0[i] <- prices[[i+1]][2768]
+  volvol[i] <- V0[i]*V0[i]
+  results<- Stochastic.deltas(M, N, d, t, mu[i], a[i], b[i], volvol[i], S0[i], V0[i])
+  delts[i,] <- results$Deltas
   
-  d1 <- (log(S0/K) + (r + sigma*sigma/2)*t)/(sigma*sqrt(t))
-  d2 <- d1 - sigma*sqrt(t)
-  BLS <- S0*pnorm(d1) - K*exp(-r*t)*pnorm(d2)
-  return(BLS) 
 }
 
+delts
--- stochastic_volatility.R
+++ stochastic_volatility.R
@@ -1,52 +1,41 @@
-Stochastic.2d <- function(M, N, t,
-                           mu, alpha, b, 
-                           volvol, S0, V0) {
-  dt <- t/N
-  S <- V <- matrix(NA,ncol=N+1, nrow=M)
-  S[,1] <- S0
-  V[,1] <- V0
-  
-  for(i in 1:N){
-    S[,i+1] <- S[,i] + 
-      mu*S[,i]*dt + 
-      sqrt(V[,i]*S[,i]) * sqrt(dt)*matrix(rnorm(M),ncol=1)
-    
-    V[,i+1] <- V[,i] + 
-      alpha*(b-V[,i])*dt + 
-      volvol*sqrt(V[,i])*sqrt(dt)*matrix(rnorm(M),ncol=1)
-  }
-  return(list("S"=S,"V"=V))
-}
-
-
-
-Stochastic <- function(M, N, t, mu, alpha, b, volvo1, S0, V0){
+Stochastic.deltas<- function(M, N, d, t, mu, a, b, volvol, S0, V0){
   ## change in time 
   dt <- t / N 
   sqdt <- sqrt(dt) 
-  S <- V <- deltas <- array(NA, dim = c(M, N + 1, d)) 
+  S <- V <- Sh <- array(NA, dim = c(M, N + 1)) 
   
   ## Set initial values 
-  S[,1,] <- S0 
-  V[,1,] <- V0 
+  S[,1] <- Sh[,1] <- S0 
+  V[,1] <- V0 
   
   ## Simulate paths 
   for(i in 1:N){
-    Z1 <- Z2 <- matrix(rnorm(d * M), ncol = M) 
+    Z1 <- Z2 <- matrix(rnorm(M), ncol = 1) 
     
-    S[,i+1,] <- S[,i,] + mu*S[,i,]*dt +
-      sqrt(V[,i,])*S[,i,]*sqdt*Z1
+    S[,i+1] <- S[,i] + mu*S[,i]*dt + sqrt(V[,i])*S[,i]*sqdt*Z1
     
-    V[,i+1,] <- V[,i,] + alpha*(b - V[,i,])*dt +
-      volvol*sqrt(V[,i,])*sqdt*Z2
+    V[,i+1] <- V[,i] + a*(b - V[,i])*dt +
+      V[i]*sqrt(V[,i])*sqdt*Z2
     
-    ## Calculate deltas 
-    ttm <- t - dt * (i - 1) 
-    deltas[,i,] <- Delta(S[,i,], K, 0.05, ttm, volvo1) 
+    Zmat <- Z1[i]
+    Sh[,i+1] <- (S[,i] + 0.01) * exp(mu * dt + sqrt(dt) * t(V0 * Z1)) 
+    
   }
-  deltas[,N + 1,] <- Delta(X[,N+1,], K, 0.05, 0, volvo1) 
   
-  return(list('S' = S, 'Z' = Z, 'Deltas' = deltas))
+  deltas <- call_put <- array(NA, dim = c(M)) 
+  for(i in 1:M){
+    ## Find the deltas for put and call options 
+    ## We take the max delta for eacH time step for each option 
+    ## create dataframe containing whether or not it's reccomended 
+    ## to put or call at that period in time. 
+    call_delta <- Delta(eput, S[i,], Sh[i,]) 
+    put_delta <- Delta(ecall, S[i,], Sh[i,]) 
+    call_put[i] <- ifelse(call_delta > put_delta, 'call', 'put') 
+    deltas[i] <- pmax(call_delta, put_delta)/300
+    
+  }
+  
+  return(list('S' = S, 'V' = V, 'Deltas' = deltas))
 }
 
 
